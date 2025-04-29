697071d3dd3f6eac548d5e53ff5aa68d039e66e9
Arka <38325426+ShamsParvezArka@users.noreply.github.com>
2025-04-30 02:16:11 +0600

    [new] project build system added

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
+
+set cl_general= /nologo /Zi
+set cl_include= /I".\include"
+set cl_link=    /link /libpath:".\lib" ".\lib\raylib.lib"   
+set cl_lib=     opengl32.lib kernel32.lib user32.lib gdi32.lib winmm.lib msvcrt.lib shell32.lib
+set cl_exclude= /NODEFAULTLIB:libcmt
+set cl_target=  [Place_Holder] ... 
+
+call cl %cl_target% %cl_general% %cl_include% %cl_link% %cl_lib% %cl_exclude%
+
+
