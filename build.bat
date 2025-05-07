70dc028615a5d1a2a9d75eb289df8e4fcc58ea5f
Arka <38325426+ShamsParvezArka@users.noreply.github.com>
2025-05-07 20:49:14 +0600

    [fix] compatible modular build

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
 
-set cl_general= /nologo /Zi
+set cl_general= /nologo /ZI
+set cl_debug= /Fd:debug\ /Fo:debug\\
 set cl_include= /I".\include"
 set cl_link=    /link /libpath:".\lib" ".\lib\raylib.lib"   
 set cl_lib=     opengl32.lib kernel32.lib user32.lib gdi32.lib winmm.lib msvcrt.lib shell32.lib
 set cl_exclude= /NODEFAULTLIB:libcmt
-set cl_target=  kbd_hook.c canvas.c
-set cl_output=  /Fe:Paintgg.exe
+set cl_target=  src\win32_main.c ^
+                src\win32_kbd_hook.c ^
+                src\canvas.c
+set cl_output=  /Fe:debug\Paintgg.exe
 
-call cl %cl_output% %cl_target% %cl_general% %cl_include% %cl_link% %cl_lib% %cl_exclude%
-
-
+call cl %cl_output% %cl_target% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
