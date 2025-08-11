4509ad6bf8eb5bfc668be36e001ba6bf789ccb91
ShamsParvezArka <parvez6862@gmail.com>
2025-08-11 21:14:32 +0600

    [fix] dll import

--- build.bat
+++ build.bat
@@ -1,25 +1,42 @@
 @echo off
 call "%SystemDrive%\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
 
-:: ============= Pre-Configuration =============
+:: --- Build Type -----------------------
+set debug=1
+set release=0
+set msvc=1
+set clang=0
+
+:: --- Pre Configuration ---------------- 
 if not exist debug mkdir debug
 if not exist assets mkdir assets
 
-:: ============= Flags =============
+:: --- Unpack Arguments ------------------
+if "%debug%"=="1"   set release=0 && echo [debug mode]
+if "%release%"=="1" set debug=0 && echo [release mode]
+if "%msvc%"=="1"    set clang=0 && echo [msvc compile]
+
+:: --- Flags ----------------------------
 :: /F4194304 --> add this to the cl_general to restrict MAX_STACK allocation 
-set cl_general= /nologo /ZI /FC /F4194304
+set cl_general= /nologo /ZI /FC
 set cl_debug=   /Fd:debug\ /Fo:debug\\
 
-:: ============= Include & Libs =============
+:: --- Include & Libs -------------------
 set cl_include= /I".\include"
 set cl_link=    /link /libpath:".\lib"
-set cl_lib=      SDL3.lib User32.lib Gdi32.lib SDL3_Image.lib Shell32.lib
+set cl_lib=     User32.lib Shell32.lib Gdi32.lib SDL3.lib SDL3_Image.lib 
 set cl_exclude=
 
-:: ============= Source & Output =============
-set cl_target=  src\win32_main.c ^
+:: --- Source & Output ------------------
+set cl_source=  src\win32_main.c ^
 								src\zink_renderer.c
 set cl_output=  /Fe:debug\zink.exe
 
-:: ============= Compile =============
-call cl %cl_output% %cl_target% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
+:: --- Compile --------------------------
+if "%msvc%"=="1" call cl %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
+
+:: --- Post Configuration ---------------
+echo Copying DLL...
+set dll_source=.\lib
+set dll_destination=.\debug
+copy /Y "%dll_source%\*.dll" "%dll_destination%" > NUL
