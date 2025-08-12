a1245169f4212b05751b58c7e2e8f74b4995c18d
ShamsParvezArka <parvez6862@gmail.com>
2025-08-12 21:49:51 +0600

    [new] debug and realse mode

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 call "%SystemDrive%\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
 
 :: --- Build Type -----------------------
-set debug=1
-set release=0
+set debug=0
+set release=1
 set msvc=1
 set clang=0
 
@@ -14,7 +14,7 @@ if not exist assets mkdir assets
 :: --- Unpack Arguments ------------------
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
-if "%msvc%"=="1"    set clang=0 && echo [msvc compile]
+if "%msvc%"=="1"    set clang=0 && echo [msvc compile] 
 
 :: --- Flags ----------------------------
 :: /F4194304 --> add this to the cl_general to restrict MAX_STACK allocation 
@@ -33,7 +33,8 @@ set cl_source=  src\win32_main.c ^
 set cl_output=  /Fe:debug\zink.exe
 
 :: --- Compile --------------------------
-if "%msvc%"=="1" call cl %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
+if "%debug%"=="1"   call cl /DZINK_DEBUG_MODE %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
+if "%release%"=="1" call cl %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
 
 :: --- Post Configuration ---------------
 echo Copying DLL...
