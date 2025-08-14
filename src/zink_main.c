47dc5ce659029cc9c99f23dbf30c41ae8cdc29f1
ShamsParvezArka <parvez6826@gmail.com>
2025-08-14 19:31:45 +0600

    [fix] platform specific header inclusion

--- src/zink_main.c
+++ src/zink_main.c
@@ -1,6 +1,10 @@
 #include "zink_includes.h"
 
-#include "zink_win32_platform.c"
+#ifdef _WIN32
+  #include "zink_win32_platform.c"
+#elif __linux__
+  #include "zink_linux_platform.c"
+#endif
 #include "zink_renderer.c"
 
 #ifdef _WIN32
