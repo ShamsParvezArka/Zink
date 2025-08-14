45147e7cfdb069554ddbaa3c5296fec0dcd8b2fe
ShamsParvezArka <parvez6826@gmail.com>
2025-08-14 17:25:11 +0600

    [fix] unity build entry point

--- build.bat
+++ build.bat
@@ -1,43 +1,43 @@
 @echo off
 call "%SystemDrive%\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL 2>&1
 
-:: --- Build Type -----------------------
+:: --- Build Type ---------------------------------------------------------
 set debug=0
 set release=1
 set msvc=1
 set clang=0
 
-:: --- Pre Configuration ---------------- 
+:: --- Pre Configuration --------------------------------------------------
 if not exist debug mkdir debug
 if not exist assets mkdir assets
 
-:: --- Unpack Arguments ------------------
+:: --- Unpack Arguments ----------------------------------------------------
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
 if "%msvc%"=="1"    set clang=0 && echo [msvc compile] 
 
-:: --- Flags ----------------------------
+:: --- Flags --------------------------------------------------------------
 :: /F4194304 --> add this to the cl_general to restrict MAX_STACK allocation 
 set cl_general= /nologo /ZI /FC
 set cl_debug=   /Fd:debug\ /Fo:debug\\
 
-:: --- Include & Libs -------------------
+:: --- Include & Libs -----------------------------------------------------
 set cl_include= /I".\include"
 set cl_link=    /link /libpath:".\lib"
 set cl_lib=     User32.lib Shell32.lib Gdi32.lib SDL3.lib SDL3_Image.lib 
 set cl_exclude=
 
-:: --- Source & Output ------------------
-set cl_source=  src\win32_main.c ^
-								src\zink_renderer.c
+:: --- Source & Output ----------------------------------------------------
+set cl_source=  src\zink_main.c 
 set cl_output=  /Fe:debug\zink.exe
 
-:: --- Compile --------------------------
+:: --- Compile ------------------------------------------------------------
 if "%debug%"=="1"   call cl /DZINK_DEBUG_MODE %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
 if "%release%"=="1" call cl %cl_output% %cl_source% %cl_general% %cl_debug% %cl_include% %cl_link% %cl_lib% %cl_exclude%
 
-:: --- Post Configuration ---------------
+:: --- Post Configuration -------------------------------------------------
 echo Copying DLL...
 set dll_source=.\lib
 set dll_destination=.\debug
 copy /Y "%dll_source%\*.dll" "%dll_destination%" > NUL
+echo Done.
