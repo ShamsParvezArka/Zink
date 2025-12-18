5c44cabcbb0195d07a64c2ce6b62fe1aba916406
ShamsParvezArka <parvez6826@gmail.com>
2025-12-18 23:28:54 +0600

    [new] prototype algo for drawing filled circle

--- src/zink_utils.c
+++ src/zink_utils.c
@@ -55,3 +55,27 @@ ZINK_DrawCircle(SDL_Renderer *renderer, I32 center_x, I32 center_y, I32 radius)
 		}
 	}
 }
+
+_internal void
+ZINK_DrawCircleFilled(SDL_Renderer *renderer, I32 center_x, I32 center_y, I32 radius)
+{
+	I32 x = 0;
+	I32 y = radius;
+	I32 midpoint = 5 - 4 * radius;
+
+	while (x <= y)
+	{
+		if (midpoint > 0)
+		{
+			SDL_RenderLine(renderer, center_x - x, center_y - y, center_x + x, center_y - y);
+			SDL_RenderLine(renderer, center_x - x, center_y + y, center_x + x, center_y + y);			
+			y--;
+			midpoint -= 8 * y;
+		}
+		SDL_RenderLine(renderer, center_x - y, center_y - x, center_x + y, center_y - x);
+		SDL_RenderLine(renderer, center_x - y, center_y + x, center_x + y, center_y + x);
+		
+		x++;
+		midpoint += 8 * x + 4;
+	}
+}
