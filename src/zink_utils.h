f4799dc808e0a77f831710cbbdf76b8093c91965
ShamsParvezArka <parvez6826@gmail.com>
2026-01-12 20:15:30 +0600

    fix/new -> deprecated fn marked, new erasing fn
    deprecated fn is kept for backward compatibility

--- src/zink_utils.h
+++ src/zink_utils.h
@@ -5,7 +5,8 @@ _internal F32  ZINK_LinearInterpolate(F32 point_a, F32 point_b, F32 factor);
 _internal F32  ZINK_Clamp(F32 value, F32 min, F32 max);
 _internal F32  ZINK_GetDeltaTime();
 _internal void ZINK_DrawCircle(SDL_Renderer *renderer, I32 center_x, I32 center_y, I32 radius);
-_internal void ZINK_DrawCircleFilled(SDL_Renderer *renderer, I32 center_x, I32 center_y, I32 radius);
+_internal void ZINK_DrawCircleFilledDeprecated(SDL_Renderer *renderer, I32 center_x, I32 center_y, I32 radius);
 _internal void ZINK_DrawCircleFilledCPU(U32 *pixels, I32 pitch, I32 width, I32 height, I32 center_x, I32 center_y, I32 radius);
+_internal void ZINK_EraseCircleFilledCPU(U32 *pixels, U32 *original, I32 pitch, I32 width, I32 height, I32 center_x, I32 center_y, I32 radius);
 
 #endif // ZINK_UTILS_H
